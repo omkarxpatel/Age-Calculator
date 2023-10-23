edd681101eb733e26c4ae6776360afb88e2376ba
omkarxpatel <bendonveryt@gmail.com>
2023-10-22 17:30:24 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    $("#submit").click(function(e){
+        e.preventDefault();
+    
+        var input = $("#dob-input").val();
+        var dob = new Date(input);
+        save(dob);
+        renderAgeLoop();
+    });
+
+    $("#reset").click(function (e) {
+        e.preventDefault();
+        resetDob();
+        location.reload();
+    });
+
+    function save(dob)
+    {
+        localStorage.dob = dob.getTime();
+    };
+
+    function resetDob() {
+        localStorage.removeItem("dob");
+    }
+
+    function load()
+    {
+        var dob;
+        if (dob = localStorage.getItem("dob"))
+        {
+            return new Date(parseInt(dob));
+        }
+        return -1;
+    };
+
+    function renderAgeLoop()
+    {
+        var dob = load();
+        $("#choose").css("display", "none");
+        $("#timer").css("display", "block");
+
+        setInterval(function(){
+            var age = getAge(dob);
+            $("#age").html(age.year + "<sup>." + age.ms + "</sup>");
+
+            var yearsLeft = averageAgeUSA - age.year;
+            var monthsLeft = Math.round((yearsLeft - Math.floor(yearsLeft)) * 12);
+
+            $("#time-left").html(yearsLeft.toFixed(0) + "<sup>." + monthsLeft + "</sup>");
+        }, 100);
+    };
+
+    function renderChoose()
+    {
+        $("#choose").css("display", "block");
+    };
+
+    function getAge(dob){
+        var now       = new Date;
+        var duration  = now - dob;
+        var years     = duration / 31556900000;
+        
+        var majorMinor = years.toFixed(9).toString().split('.');
+        
+        return {
+            "year": majorMinor[0],
+            "ms": majorMinor[1]
+        };
+    };
+
+    function main() {
+        if (load() != -1)
+        {
+            renderAgeLoop();
+        } else {
+            renderChoose();
+        }
+    };
+    main();
+});
